3a4944750f386bb0bc0a9ef4302beeb8dc0e0434
William Leung <leungwk25@gmail.com>
2018-02-21 00:03:31 -0800

    New Boilerplate

---
+++ app/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Including our ListContainer and Container components
+import ListContainer from "./components/ListContainer";
+import Container from "./components/Container";
+
+// Creating an App component which renders a ListContainer inside of a Container
+const App = () => (
+  <Container>
+    <ListContainer />
+  </Container>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
